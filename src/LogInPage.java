255f0ed2128a7d3dadd88afeecb7e105e7083036
halukenes <halukenes97@gmail.com>
2018-04-11 12:41:49 +0300

    Finished design of LogInPage.java

--- src/LogInPage.java
+++ src/LogInPage.java
@@ -1,10 +1,19 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LogInPage {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -34,8 +43,56 @@ public class LogInPage {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 455, 332);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblLogn = new JLabel("Login");
+		lblLogn.setFont(new Font("Calibri", Font.BOLD, 24));
+		lblLogn.setBounds(50, 40, 64, 26);
+		frame.getContentPane().add(lblLogn);
+		
+		JLabel lblNewLabel = new JLabel("Please fill in your credentials to login.");
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 13));
+		lblNewLabel.setBounds(50, 66, 210, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(50, 118, 210, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Calibri", Font.BOLD, 12));
+		lblUsername.setBounds(50, 102, 51, 14);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Calibri", Font.BOLD, 12));
+		lblPassword.setBounds(50, 160, 51, 14);
+		frame.getContentPane().add(lblPassword);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(50, 176, 210, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnLogin.setBackground(new Color(0, 102, 255));
+		btnLogin.setForeground(Color.BLACK);
+		btnLogin.setBounds(50, 228, 89, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		JButton btnSignin = new JButton("Sign Up");
+		btnSignin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSignin.setBounds(305, 228, 89, 23);
+		frame.getContentPane().add(btnSignin);
 	}
-
 }
