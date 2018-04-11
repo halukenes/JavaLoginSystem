3290fe2bab29a019721d3f97497d302b3b893850
halukenes <halukenes97@gmail.com>
2018-04-11 18:56:31 +0300

    Changed codein CreatedDB.java to fix bug in exporting as a runnable .jar file

--- src/CreateDB.java
+++ src/CreateDB.java
@@ -7,18 +7,18 @@ public class CreateDB {
 	public static final String JDBC_URL = "jdbc:derby:zadb;create=true";
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Connection connection = DriverManager.getConnection(JDBC_URL);
-		
-		//if (connection == null) {
-			Class.forName(DRIVER);
-			//connection.createStatement().execute("create table users(username varchar(20), password varchar(20), email varchar(25), telNu varchar(20))");
-			//connection.createStatement().execute("DELETE FROM users");
-			//connection.createStatement().execute("drop table users");
-			System.out.println("channels table created and records succesfully inserted ...");
-	//	}
-
 		LogInPage logInPage = new LogInPage();
 		logInPage.frame.setVisible(true);
+		
+		Class.forName(DRIVER);
+		Connection connection = DriverManager.getConnection(JDBC_URL);
+	    
+		connection.createStatement().execute("create table users(username varchar(20), password varchar(20), email varchar(25), telNu varchar(20))");
+	    //connection.createStatement().execute("DELETE FROM users");
+		//connection.createStatement().execute("drop table users");
+		System.out.println("channels table created and records succesfully inserted ...");
+
+		
 		
 		
 	}
